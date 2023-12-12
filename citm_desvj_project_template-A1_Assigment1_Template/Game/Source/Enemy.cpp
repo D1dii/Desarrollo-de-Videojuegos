1b7e0003b79873c0d8a6f4a677f98a8126f2f9a7
D1dii <didacgp123@gmail.com>
2023-12-12 11:20:03 +0100

    First enemy

--- citm_desvj_project_template-A1_Assigment1_Template/Game/Source/Enemy.cpp
+++ citm_desvj_project_template-A1_Assigment1_Template/Game/Source/Enemy.cpp
@@ -62,9 +62,6 @@ bool Enemy::Start()
 bool Enemy::Update(float dt)
 {
 
-	iPoint origin = iPoint(10, 136);
-	iPoint destination = iPoint(20, 134);
-
 	iPoint enemyPos = app->map->WorldToMap(position.x, position.y);
 	iPoint playerPos = app->map->WorldToMap(app->scene->GetPLayerPosition().x, app->scene->GetPLayerPosition().y);
 
@@ -73,19 +70,28 @@ bool Enemy::Update(float dt)
 	const DynArray<iPoint>* path = app->map->pathfinding->GetLastPath();
 
 
-
 	for (uint i = 0; i < path->Count(); ++i)
 	{
 		iPoint pos = app->map->MapToWorld(path->At(i)->x, path->At(i)->y);
 		app->render->DrawTexture(pathTest, pos.x, pos.y);
 	}
 
-	if (timer >= 60) {
-		iPoint pos = app->map->MapToWorld(path->At(0)->x, path->At(0)->y);
-		enemyCollider->body->SetTransform(b2Vec2(PIXEL_TO_METERS(pos.x), PIXEL_TO_METERS(pos.y)), 0);
-		timer = 0;
+	if (path->Count() > 1 && app->map->pathfinding->CreatePath(enemyPos, playerPos) != -1) {
+		iPoint pos = app->map->MapToWorld(path->At(1)->x, path->At(1)->y);
+
+		if (enemyPos.x - playerPos.x < 0 && abs(enemyPos.x - playerPos.x) > 2) {
+			enemyCollider->body->SetLinearVelocity(b2Vec2(0.1 * dt, 0.2 * dt));
+		}
+		else if (abs(enemyPos.x - playerPos.x) > 2) {
+			enemyCollider->body->SetLinearVelocity(b2Vec2(-0.1 * dt, 0.2 * dt));
+		}
+		else if (abs(enemyPos.x - playerPos.x) < 2) {
+			enemyCollider->body->SetLinearVelocity(b2Vec2(0, 0.2 * dt));
+			enemyCollider->body->SetLinearDamping(0);
+		}
 	}
-	timer++;
+
+
 
 	position.x = METERS_TO_PIXELS(enemyCollider->body->GetTransform().p.x - 5);
 	position.y = METERS_TO_PIXELS(enemyCollider->body->GetTransform().p.y - 4);
